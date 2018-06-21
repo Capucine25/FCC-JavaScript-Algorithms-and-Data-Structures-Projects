cb58b6e4d55cc12b5f8dad66d24b093bacb79c86
Capucine25 <40346652+Capucine25@users.noreply.github.com>
2018-06-21 11:30:12 -0400

    Update 05_Cash Register.js

--- 05_Cash Register.js
+++ 05_Cash Register.js
@@ -1,6 +1,6 @@
 function checkCashRegister(price, cash, cid) {
-  var change=cash-price;
-  var typeOfMoney=[0.01,0.05,0.10,0.25,1,5,10,20,100]
+  var change=(cash-price)*100;
+  var typeOfMoney=[1,5,10,25,100,500,1000,2000,10000]
   var baseCid=cid.slice(0)
   var changeToGive=[]
 
@@ -15,10 +15,15 @@ function checkCashRegister(price, cash, cid) {
   for (i=0;i<(cid.length);i++){
     cid[i].push(typeOfMoney[i])
     
-  //Also creates an array in which to add to change to give
+  //And multiplies the money in cdi by 100 to have only integers
+   cid[i][1]=cid[i][1]*100
+    
+  //And creates an array in which to add to change to give
   changeToGive.push([cid[i][0],0])
+  
+  
   }
-
+  
   //Calculate the change to give 
   for (var i=(cid.length-1);i>=0;i--){
     while ((change-cid[i][2])>=0 && cid[i][1]){
@@ -32,13 +37,13 @@ function checkCashRegister(price, cash, cid) {
   
   //Removes the type of money that doesn't need to be given
     changeToGive=changeToGive.filter(type=> type[1])
-  
+    
 //Case if there is not enough money in the register
   if (change>0){
     return {status: "INSUFFICIENT_FUNDS", change: []}
   }
-  
-//
+
+//Case if there is not money left in the register after giving change
   
 
   

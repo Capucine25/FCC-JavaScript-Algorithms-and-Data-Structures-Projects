d72e65389e5119aab047907510f56e29fd5ef311
Capucine25 <40346652+Capucine25@users.noreply.github.com>
2018-06-21 09:47:45 -0400

    Create 05_Cash Register.js

---
+++ 05_Cash Register.js
@@ -0,0 +1,15 @@
+function checkCashRegister(price, cash, cid) {
+  var change;
+  var typeOfMoney=[0.01,0.05,0.10,0.25,1,2,5,10,20,100]
+  return change;
+  
+  //Adds money type to cid array
+  for (i=0;i<(cid.length);i++){
+    cid[i].push(typeOfMoney[i])
+  }
+ 
+  while (cash-price>0)
+  {}
+  
+  console.log(cash-price)
+}

4fc88172260a240764e69c6a952e68d97887fa15
Capucine25 <40346652+Capucine25@users.noreply.github.com>
2018-06-20 21:32:19 -0400

    Update 04_Telephone Number Validator.js

--- 04_Telephone Number Validator.js
+++ 04_Telephone Number Validator.js
@@ -1,23 +1,38 @@
 function telephoneCheck(str) {
   
- //Determines if there is a country code.
+ //Determines if there is a country code. If there is, checks if it is a 1.
   if (/^[\d][\s]*[(]*[\d]{3,}.*[\d]{3,}.*[\d]{4,}/.test(str)){
     if (str[0]==1){
       str=str.replace(str[0],"");
         str=str.trim()
       }
     
-    else 
-      {return false}
+    else {
+      return false
+      
+    }
   }
   
-//Permet de s'assurer que le numero n'est pas trop long  
-str=str+"-" 
-console.log(str)
+//Checks if a ( is followed by )
+  if (str[0]=="("){
+    if (str[4]!=")"){
+      return false
+    }
+  }
+  
+//Checks if a ) comes after a (
+  if (str[3]==")"){
+    if (str[0]!="("){
+      return false
+    }
+  }
 
+  
+//Allows the regular expression that follows to check if the number is too long
+str=str+"-" 
 
 return (/^[(]*[\d]{3,3}[)]*[\s|-]*[\d]{3,3}[-|\s]*[\d]{4,4}[-]$/.test(str))
   
 }
 
-console.log(telephoneCheck("(275)76227382"));
+console.log(telephoneCheck("1 555)555-5555"));
